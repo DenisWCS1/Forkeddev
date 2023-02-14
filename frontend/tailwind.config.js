124dae6ad93c80f8c90067af753237ec9273e11b
albja <92363820+albja@users.noreply.github.com>
2023-02-14 11:17:56 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('.\frontend\node_modules\tailwindcss').Config} */
 module.exports = {
-  content: ["./src//*.{jsx,js}"],
+  content: ["./src/**/*.{jsx,js}"],
   theme: {
     extend: {
       colors: {
