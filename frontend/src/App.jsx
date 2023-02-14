bc812c86cfe5eae3c804f9b767ea827f35ec5e35
albja <92363820+albja@users.noreply.github.com>
2023-02-14 11:21:05 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,20 @@ function App() {
       <p className="bg-orange-700 text-5xl mr-6 my-5">
         J'utilise le CSS de base de TAILWIND
       </p>
-      <p className="bg-dark-100 text-5xl mr-6 my-5">J'utilise le CSS Perso</p>      
+      <p className="bg-dark-100 text-5xl mr-6 my-5">J'utilise le CSS Perso</p>
       <p className="bg-blueBlack-100 text-5xl mr-6 my-5">
-      J'utilise le CSS Perso</p>      
+      J'utilise le CSS Perso
+      </p>
       <p className="bg-blueDuck-100 text-5xl mr-6 my-5">J'utilise le CSS Perso
-      </p>      
+      </p>
       <p className="bg-blueSimple-100 text-5xl mr-6 my-5">
       J'utilise le CSS Perso
-      </p>      
+      </p>
       <p className="bg-turquoise-100 text-5xl mr-6 my-5">J'utilise le CSS Perso
-      </p>      
+      </p>
       <p className="bg-greySimple-100 text-5xl mr-6 my-5">
       J'utilise le CSS Perso
-      </p>      
+      </p>
       <p className="bg-whiteSimple-100 text-5xl mr-6 my-5">
       J'utilise le CSS Perso
       </p>
